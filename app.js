8fcd19434a7c7ae164499fbf855f4bb9b8860ca0
Jingnu An <78512122+JingnvAn@users.noreply.github.com>
2022-05-01 22:04:43 -0400

    use environment variable (#5)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios')
 
@@ -19,7 +20,8 @@ const layouts = require('express-ejs-layouts');
  * connect to the database
  *************************/
 const mongoose = require('mongoose');
-const mongodb_URI = 'mongodb://localhost:27017/bunnybearbao'
+// const mongodb_URI = 'mongodb://localhost:27017/bunnybearbao';
+const mongodb_URI = process.env.mongodb_URI;
 mongoose.connect(mongodb_URI, { useNewUrlParser: true} );
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
